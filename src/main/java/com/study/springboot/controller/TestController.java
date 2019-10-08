7bdedb478811c09a4f94256b7919b71a0b0c1fdc
zhoutang <zhou.tang@ronds.com.cn>
2019-10-08 11:46:27 +0800

    提交内容：创建spring boot项目

---
+++ src/main/java/com/study/springboot/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.study.springboot.controller;
+
+import com.study.springboot.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private Student student;
+
+    @GetMapping (value = "/test")
+    public String test(){
+        return student.getName()+":"+student.getAge()+":"+student.getScore();
+    }
+
+
+
+}
